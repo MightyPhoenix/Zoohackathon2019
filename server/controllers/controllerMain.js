7eaa0f3aa913f1f1cbee0843737fdb0f3e48bfdb
vikravager <vikravager@github.com>
2019-11-09 15:31:35 +0530

    bug -vik

--- server/controllers/controllerMain.js
+++ server/controllers/controllerMain.js
@@ -29,12 +29,143 @@ router.get('/signup',(req, res, next)=>{
 });
 
 //POST
-router.post('/login',(req,res,next)=>{
-    email = req.body.email;
-    password = req.body.password;
 
 
 
+//localstrategy passport
+
+passport.use(new localStrategy(function(username,password,done){
+       
+    User.getUserByUsername(username,function(err,user){
+            if(err) throw err;
+            if(!user){
+                    return done(null,false,{message : 'Unknown User'});
+
+            }
+
+            User.comparedPassword(password,user.password,function(err,isMatch){
+                    if(err) throw err;
+                    if(isMatch){
+                            
+                            
+                            return done(null,user);
+                            
+                    }
+                    else{
+                            return done(null,false,{message : 'Invalid passpword'})
+                    }
+            });
+    });
+}));
+    passport.serializeUser(function(user, done) {
+
+            console.log('userId: '+user._id);
+            done(null, user._id);
+    });
+    
+    passport.deserializeUser(function(id, done) {
+            User.getUserById(id, function(err, user) {
+            done(err, user);
+            });
+    });
+
+router.post('/login',passport.authenticate('local',
+    { successRedirect: '/dashboard',
+    failureRedirect: '/',
+    failureFlash : true }),
+    function(req,res){
+    res.redirect('/dashboard');
+});
+
+
+//localstrategy ends
+
+
+
+
+function loggedIn(req,res,next){
+    if(req.isAuthenticated()){
+            return next();
+    }
+    else{
+            res.redirect('/');
+    }
+}
+function loggedOut(req,res,next){
+    if(!req.isAuthenticated()){
+            return next();
+    }
+    else{
+            res.redirect('/dashboard');
+    }
+}
+
+// dashboard
+
+
+router.get('/dashboard',loggedIn,function(req,res){
+    var userType= "";
+    if(req.user.typeUser == "adm") {
+            userType = "Admin";
+            var resume = "";
+           // const adminDetails;
+            var name = req.user.username;
+            // var admin = new Admin();
+            Admin.findOne({email : name},function(err,doc){
+                    if(!err){
+                            const adminDetails = doc;
+                            res.render('Admin/dashboard',{
+                                userType : userType,
+                                name : name,
+                             
+                                });
+                            
+                    }
+                    else {
+                            console.log(err);
+                    }
+            });
+            
+          
+    }
+    else 
+    if(req.user.typeUser == "wdn") {
+        userType = "Warden";
+       
+       // const adminDetails;
+        var name = req.user.username;
+        // var admin = new Admin();
+        Warden.findOne({email : name},function(err,doc){
+                if(!err){
+                        const adminDetails = doc;
+                        res.render('Warden/dashboard',{
+                            userType : userType,
+                            name : name,
+                
+                            });
+                        
+                }
+                else {
+                        console.log(err);
+                }
+        });
+        
+      
+        }
+        else
+        if(req.user.userType = "onr"){
+            if (err) throw err;
+
+            var name = req.user.username;
+
+            Owner.findOne({email : name},function(err,doc){
+                if (err) throw err;
+                res.render('Owner/dashboard',{
+                    userType : userType,
+                    doc : doc
+                });
+            })
+        }
 
 });
 
