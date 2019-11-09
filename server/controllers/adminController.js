1e894a8a383247126ebb4b2b648d38be1cbf0f33
Atiab <atiabkalam@gmail.com>
2019-11-09 16:21:11 +0530

    Backend for new Warden page and admin logged in function created -Atiab

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -5,8 +5,18 @@ const mongoose = require('mongoose');
 
 //Models
 var Admin = mongoose.model('Admin');
+var Warden = mongoose.model('Warden');
 var User = mongoose.model('User');
 
+
+function loggedInAdm(req,res,next){
+    if(req.isAuthenticated() && req.user.typeUser=='adm'){
+            return next();
+    }
+    else{
+            res.redirect('/');
+    }
+}
 
 //Routes
 router.get('/register',function(req,res){
@@ -37,5 +47,35 @@ router.post('/register',function(req,res){
     });
 });
 
+router.get('/newWarden',loggedInAdm,function(req,res){
+    res.render('Admin/newWarden');
+});
+
+router.post('/newWarden',loggedInAdm,function(req,res){
+
+    var email = req.body.email;
+    var password = req.body.password;
+    var name = req.body.name;
+    var state = req.body.state;
+    console.log(`email : ${email}`);
+    var warden = new Warden();
+    var user = new User({
+        username : email,
+        password : password,
+        typeUser : 'war'
+    });    
+
+    User.createUser(user, function(err){
+        if(err) throw err;
+
+        warden.name = name;
+        warden.email = email;
+        warden.state = state;
+        warden.save(function(err,doc){
+            if(err) throw err;
+            res.send('Registered..');
+        });
+    });
+});
 
 module.exports = router;
