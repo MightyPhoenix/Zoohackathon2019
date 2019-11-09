f97ac24cb1c551e857348f76ec85c1171d380841
vikravager <vikravager@github.com>
2019-11-09 10:36:23 +0530

    First commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+let app = express();
+
+let publicPath = path.join(__dirname,'/../public/');
+
+app.use(express.static(publicPath));
+
+let server = http.createServer(app);
+
+
+
+let port = process.env.PORT || 3300;
+
+server.listen(port,function(err){
+    if(err) throw err;
+    console.log(`server is running at port : ${port}`);
+})
