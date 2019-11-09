03898afe7fb7b710ba7f9f33e120015e1ef23ed3
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2019-11-09 11:10:04 +0530

    "Server Update -Agnibesh"

---
+++ server/controllers/controllerMain.js
@@ -0,0 +1,11 @@
+// Modules
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+
+//MODELS
+
+
+// GET
--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 let app = express();
 
+// STATIC FOLDER
 let publicPath = path.join(__dirname,'/../public/');
-
 app.use(express.static(publicPath));
-
+// SERVER
 let server = http.createServer(app);
+
+// VIEW ENGINE
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//ROUTES
+const mainController = require('./controllers/controllerMain');
+
 
 
 
