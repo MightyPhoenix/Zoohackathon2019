be59dac12830af31c6a6e7774bf84abb0c5c10ca
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2019-11-09 12:42:01 +0530

    "Signup Login Page Added -Agnibesh"

--- server/controllers/controllerMain.js
+++ server/controllers/controllerMain.js
@@ -13,6 +13,13 @@ router.get('/',(req, res, next)=>{
     res.render('index', { title: 'Index' });
 });
 
+router.get('/login',(req, res, next)=>{
+    res.render('login', { title: 'login' });
+});
+
+router.get('/signup',(req, res, next)=>{
+    res.render('signup', { title: 'Registration' });
+});
 
 
 module.exports = router;
